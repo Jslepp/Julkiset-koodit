ff25f5bb9b3eba0fc59e219013fbb3a5e455390c
Santeri Leppä <jslepp@utu.fi>
2021-02-16 16:47:53 +0200

    Muokattiin laiva tiedostoa

--- Java/Olio-ohjelmoinnin perusteet/Demo 1/tehtava1/Laiva.java
+++ Java/Olio-ohjelmoinnin perusteet/Demo 1/tehtava1/Laiva.java
@@ -1,47 +1,28 @@
-import java.util.*;
+package tehtava1;
 
-class Laiva {
-    String nimi;
-    int pituus;
-    int syvays;
-    double nopeus;
-
-
-    public Laiva(String nimi, int pituus, int syvays, double nopeus) {
-        this.nimi = nimi;
-        this.pituus = pituus;
-        this.syvays = syvays;
-        this.nopeus = nopeus;
-    }
-
-    public static double matkanLaskeminen(double matka, double nopeus) {
-        return matka/(nopeus/2);
-    }
-
-    public static void main(String[] args) {
-        Scanner lukija = new Scanner(System.in);
-        System.out.println("Anna laivan pituus metreinä: ");
-        int pituus = Integer.valueOf(lukija.nextLine());
-        System.out.println("Anna laivan syväys: ");
-        int syvays = Integer.valueOf(lukija.nextLine());
-        System.out.println("Anna laivan nopeus solmuina: ");
-        double nopeus = lukija.nextDouble();
-        System.out.println("Laivan nimi: ");
-        String nimi = lukija.next();
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("Laiva: " + nimi + "\n"
-        + "Pituus: " + pituus + " metriä" + "\n"
-        + "Syväys: " + syvays + "\n"
-        + "Nopeus: " + nopeus + " solmua");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("Syötä matka metreinä jolle haluat laskea laivalla kuluvan matkan. ");
-        double matka = lukija.nextInt();
-        System.out.println("Kuluva aika: " + matkanLaskeminen(matka,nopeus) + "sekunttia");
-        lukija.close();
-    } 
+public class Laiva {
+	String nimi;
+	double pituus;
+	double syvays;
+	double nopeus;
+	
+	/*
+	 * @param pituus Laivan pituus metreinä
+	 * @param syvays Laivan syväys metreinä
+	 * @param nopeus Laivan nopeus solmuina
+	 */
+	public Laiva(String nimi, double pituus, double syvays, double nopeus) {
+		this.nimi = nimi;
+		this.pituus = pituus;
+		this.syvays = syvays;
+		this.nopeus = nopeus;
+	}
+	
+	/*
+	 * @param matka Laivan kulkema matka meripeninkulmina (1 mpk = 1,852 km)
+	 * @return Laivan kulkeman matkan kesto tunteina
+	 */
+	public double laskeKesto(double matka) {
+		return matka / nopeus;
+	}
 }

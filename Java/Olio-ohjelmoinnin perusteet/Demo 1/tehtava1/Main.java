fd043d0930cbd115ffbd33f2aae57873f2de5c90
Santeri Leppä <jslepp@utu.fi>
2021-02-16 16:52:03 +0200

    Vaihdettiin main tiedosto oikeaan main tiedostoon

--- Java/Olio-ohjelmoinnin perusteet/Demo 1/tehtava1/Main.java
+++ Java/Olio-ohjelmoinnin perusteet/Demo 1/tehtava1/Main.java
@@ -1,13 +1,8 @@
 package tehtava1;
 
 public class Main {
-
-	public static void main(String[] args) {
-		Laiva laiva1 = new Laiva("Titanic", 269.0, 10.0, 23.0);
-		Laiva laiva2 = new Laiva("Mayflower", 27.5, 4.0, 3.0);
-		
-		System.out.println(laiva1.nimi + ": "+ laiva1.laskeKesto(30));
-		System.out.println(laiva2.nimi + ": "+ laiva2.laskeKesto(30));
-	}
-	
+    public static void main(String[] args) {
+        Laiva laiva1 = new Laiva("Titanic", 100, 20, 10);
+        System.out.println(laiva1.nimi + ": " + Laiva.matkanLaskeminen(30));
+    } 
 }
